3d757729eaffa3e4cc99e23348835e8fc24603bf
Axselll <anasahetapi99@gmail.com>
2021-02-19 21:49:43 +0900

    lesson 8 done

--- public/sandbox.js
+++ public/sandbox.js
@@ -1,2 +1,22 @@
 "use strict";
-console.log("test");
+var greet;
+greet = function () {
+    console.log("hello again");
+};
+// use question mark ( ? ) if thers is no default value for optional paramater [c?]
+// void is a complete lack of any value
+var add = function (a, b, c) {
+    if (c === void 0) { c = 10; }
+    console.log(a + b);
+    console.log(c);
+};
+// pass third param to overwrite default value
+add(1, 2);
+// add(1, 2, 3);
+// explicitly declare the return type of the number put :number or etc behind parantheses
+var minus = function (a, b) {
+    return a + b;
+};
+var res = minus(10, 1);
+// dont ever try it
+// res = "";
--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,1 +1,26 @@
-console.log("test");
+let greet: Function;
+
+greet = () => {
+	console.log("hello again");
+};
+
+// use question mark ( ? ) if thers is no default value for optional paramater [c?]
+// void is a complete lack of any value
+const add = (a: number, b: number, c: number | string = 10): void => {
+	console.log(a + b);
+	console.log(c);
+};
+
+// pass third param to overwrite default value
+add(1, 2);
+// add(1, 2, 3);
+
+// explicitly declare the return type of the number put :number or etc behind parantheses
+const minus = (a: number, b: number): number => {
+	return a + b;
+};
+
+let res = minus(10, 1);
+
+// dont ever try it
+// res = "";
