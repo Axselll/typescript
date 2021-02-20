86c50904a28e95c6e46cff72e8e18d3604b0b204
Axselll <anasahetapi99@gmail.com>
2021-02-20 19:58:38 +0900

    lesson 9 done

--- public/sandbox.js
+++ public/sandbox.js
@@ -1,22 +1,7 @@
 "use strict";
-var greet;
-greet = function () {
-    console.log("hello again");
+var logDetails = function (uid, item) {
+    console.log(item + " has a uid of " + uid);
 };
-// use question mark ( ? ) if thers is no default value for optional paramater [c?]
-// void is a complete lack of any value
-var add = function (a, b, c) {
-    if (c === void 0) { c = 10; }
-    console.log(a + b);
-    console.log(c);
+var greet = function (user) {
+    console.log(user.name + " say hello");
 };
-// pass third param to overwrite default value
-add(1, 2);
-// add(1, 2, 3);
-// explicitly declare the return type of the number put :number or etc behind parantheses
-var minus = function (a, b) {
-    return a + b;
-};
-var res = minus(10, 1);
-// dont ever try it
-// res = "";
