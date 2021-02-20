6e0ce7e2b9bf300e9f1a01f1827dde489e98af40
Axselll <anasahetapi99@gmail.com>
2021-02-20 21:04:25 +0900

    i dunno hehe

--- public/app.js
+++ public/app.js
@@ -16,13 +16,3 @@ var invTwo = new Invoice("luigi", "work for the website", 200);
 var invoices = [];
 invoices.push(invOne, invTwo);
 console.log(invoices);
-var form = document.querySelector(".new-item-form");
-// inputs
-var type = document.querySelector("#type");
-var tofrom = document.querySelector("#tofrom");
-var details = document.querySelector("#details");
-var amount = document.querySelector("#amount");
-form.addEventListener("submit", function (e) {
-    e.preventDefault();
-    console.log(type.value, tofrom.value, details.value, amount.valueAsNumber);
-});
