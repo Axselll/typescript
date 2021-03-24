bcfeb522a39b9bf560fc00f596b9904a7779b097
Axselll <anasahetapi99@gmail.com>
2021-03-24 13:54:52 +0900

    lesson14 done

--- public/app.js
+++ public/app.js
@@ -1,26 +1,8 @@
-"use strict";
-//  Classes
-var Invoice = /** @class */ (function () {
-    // client: string;
-    // details: string;
-    // amount: number;
-    function Invoice(client, details, amount) {
-        this.client = client;
-        this.details = details;
-        this.amount = amount;
-        // this.client = c;
-        // this.details = d;
-        // this.amount = a;
-    }
-    Invoice.prototype.format = function () {
-        return this.client + " owes \u00A3 " + this.amount + " for " + this.details;
-    };
-    return Invoice;
-}());
-var invOne = new Invoice("mario", "work for the website", 100);
-var invTwo = new Invoice("luigi", "work for the website", 200);
-var invoices = [];
+import { Invoice } from "./classes/Invoice.js";
+const invOne = new Invoice("mario", "work for the website", 100);
+const invTwo = new Invoice("luigi", "work for the website", 200);
+let invoices = [];
 invoices.push(invOne, invTwo);
-invoices.forEach(function (inv) {
+invoices.forEach((inv) => {
     console.log(inv.client, inv.amount, inv.format());
 });
--- src/app.ts
+++ src/app.ts
@@ -1,23 +1,4 @@
-//  Classes
-class Invoice {
-	// client: string;
-	// details: string;
-	// amount: number;
-
-	constructor(
-		readonly client: string,
-		private details: string,
-		public amount: number
-	) {
-		// this.client = c;
-		// this.details = d;
-		// this.amount = a;
-	}
-
-	format() {
-		return `${this.client} owes £ ${this.amount} for ${this.details}`;
-	}
-}
+import { Invoice } from "./classes/Invoice.js";
 
 const invOne = new Invoice("mario", "work for the website", 100);
 const invTwo = new Invoice("luigi", "work for the website", 200);
