c38e334152d4a8fc4626fdd65154ccffc2f21d68
Axselll <anasahetapi99@gmail.com>
2021-04-12 20:59:04 +0900

    still have errors

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,25 @@
+// Interfaces
+// interface isPerson {
+// 	name: string;
+// 	age: number;
+// 	speak(language: string): void;
+// 	spend(money: number): number;
+// }
+
+// const me: isPerson = {
+// 	name: "accel",
+// 	age: 21,
+// 	speak(language: string): void {
+// 		console.log(language);
+// 	},
+// 	spend(money: number): number {
+// 		console.log(`I Spent ${money}`);
+// 		return money;
+// 	},
+// };
+
+// console.log(me);
+
 import { Invoice } from "./classes/Invoice.js";
 
 const invOne = new Invoice("mario", "work for the website", 100);
