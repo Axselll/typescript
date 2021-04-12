3bda2762e50226f666b5a2210d043a952dd960a4
Axselll <anasahetapi99@gmail.com>
2021-04-12 21:07:13 +0900

    node must have a package.json and set the type, use browser instead

--- src/app.ts
+++ src/app.ts
@@ -1,24 +1,24 @@
 // Interfaces
-// interface isPerson {
-// 	name: string;
-// 	age: number;
-// 	speak(language: string): void;
-// 	spend(money: number): number;
-// }
+interface isPerson {
+	name: string;
+	age: number;
+	speak(language: string): void;
+	spend(money: number): number;
+}
 
-// const me: isPerson = {
-// 	name: "accel",
-// 	age: 21,
-// 	speak(language: string): void {
-// 		console.log(language);
-// 	},
-// 	spend(money: number): number {
-// 		console.log(`I Spent ${money}`);
-// 		return money;
-// 	},
-// };
+const me: isPerson = {
+	name: "accel",
+	age: 21,
+	speak(language: string): void {
+		console.log(language);
+	},
+	spend(money: number): number {
+		console.log(`I Spent ${money}`);
+		return money;
+	},
+};
 
-// console.log(me);
+console.log(me);
 
 import { Invoice } from "./classes/Invoice.js";
 
